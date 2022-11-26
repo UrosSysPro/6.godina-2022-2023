f0b6160b2a5dbde2c97e8f97f6a2ef245b3e39c7
Uros <karaleicuu@gmail.com>
2022-11-26 15:06:25 +0100

    radi parallax efekat na mapi

---
+++ core/src/org/systempro/project/platformer/TestScreen.java
@@ -0,0 +1,57 @@
+package org.systempro.project.platformer;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+import org.systempro.project.BasicScreen;
+import org.systempro.project.camera.Camera2d;
+
+public class TestScreen extends BasicScreen {
+    TiledMap map;
+    TiledMapRenderer mapRenderer;
+    Camera2d camera2d;
+
+    float x,y,width,height;
+
+    @Override
+    public void show() {
+        x=0;y=0;width=Gdx.graphics.getWidth();height=Gdx.graphics.getHeight();
+        camera2d=new Camera2d();
+        camera2d.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        camera2d.setScale(1,1);
+        camera2d.position.set(0,0,0);
+        map=new TmxMapLoader().load("platformerMap.tmx");
+
+        mapRenderer=new OrthogonalTiledMapRenderer(map);
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0,0,0,1);
+        int speed=2;
+        if(Gdx.input.isKeyPressed(Input.Keys.W))y-=speed;
+        if(Gdx.input.isKeyPressed(Input.Keys.S))y+=speed;
+        if(Gdx.input.isKeyPressed(Input.Keys.A))x+=speed;
+        if(Gdx.input.isKeyPressed(Input.Keys.D))x-=speed;
+
+        camera2d.setTranslation(x,y);
+
+        camera2d.update();
+        mapRenderer.setView(camera2d.combined4,-x-width/2,-y-height/2,width,height);
+
+        map.getLayers().get("back1").setOffsetX(-x/10);
+        map.getLayers().get("back1").setOffsetY(y/10);
+        map.getLayers().get("front1").setOffsetX(x/10);
+        map.getLayers().get("front1").setOffsetY(-y/10);
+        mapRenderer.render();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
