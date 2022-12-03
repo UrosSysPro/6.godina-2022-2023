e400acf39ac0423b48efa80abe07c44dc00c2aa6
Uros <karaleicuu@gmail.com>
2022-12-03 13:37:51 +0100

    platformer pocetak

--- core/src/org/systempro/project/platformer/TestScreen.java
+++ core/src/org/systempro/project/platformer/TestScreen.java
@@ -2,29 +2,65 @@ package org.systempro.project.platformer;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
 import org.systempro.project.BasicScreen;
 import org.systempro.project.camera.Camera2d;
+import org.systempro.project.physics2d.RectBody;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class TestScreen extends BasicScreen {
     TiledMap map;
     TiledMapRenderer mapRenderer;
     Camera2d camera2d;
+    ShapeRenderer shapeRenderer;
 
-    float x,y,width,height;
+    World world;
+    ArrayList<RectBody> walls;
+    RectBody player;
 
     @Override
     public void show() {
-        x=0;y=0;width=Gdx.graphics.getWidth();height=Gdx.graphics.getHeight();
+        shapeRenderer=new ShapeRenderer();
+
         camera2d=new Camera2d();
         camera2d.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         camera2d.setScale(1,1);
         camera2d.position.set(0,0,0);
         map=new TmxMapLoader().load("platformerMap.tmx");
+
+        world=new World(new Vector2(0,-10),false);
+        walls=new ArrayList<>();
+
+        MapObjects o=map.getLayers().get("objects").getObjects();
+
+        for(MapObject object : o){
+            if(Objects.equals(object.getName(), "spawn")){
+                float x= (float) object.getProperties().get("x");
+                float y= (float) object.getProperties().get("y");
+                player=new RectBody(world,x+10,y+10,20,20);
+                player.body.setFixedRotation(true);
+            }else{
+                float x= (float) object.getProperties().get("x");
+                float y= (float) object.getProperties().get("y");
+                float w= (float) object.getProperties().get("width");
+                float h= (float) object.getProperties().get("height");
+                RectBody wall=new RectBody(world,x+w/2,y+h/2,w,h);
+                wall.setType(BodyDef.BodyType.StaticBody);
+                walls.add(wall);
+            }
+        }
 
         mapRenderer=new OrthogonalTiledMapRenderer(map);
     }
@@ -32,22 +68,39 @@ public class TestScreen extends BasicScreen {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0,0,0,1);
-        int speed=2;
-        if(Gdx.input.isKeyPressed(Input.Keys.W))y-=speed;
-        if(Gdx.input.isKeyPressed(Input.Keys.S))y+=speed;
-        if(Gdx.input.isKeyPressed(Input.Keys.A))x+=speed;
-        if(Gdx.input.isKeyPressed(Input.Keys.D))x-=speed;
 
-        camera2d.setTranslation(x,y);
+        Vector2 speed=player.getVelocity();
+        if(Gdx.input.isKeyJustPressed(Input.Keys.W))speed.y=10;
+        if(Gdx.input.isKeyPressed(Input.Keys.A))speed.x=-5;
+        if(Gdx.input.isKeyPressed(Input.Keys.D))speed.x=5;
+        if(speed.y<-1)speed.y-=10*delta;
+        player.setVelocity(speed);
 
+        world.step(delta,10,10);
+
+        float x=player.getPosition().x;
+        float y=player.getPosition().y;
+        float width=Gdx.graphics.getWidth();
+        float height=Gdx.graphics.getHeight();
+        camera2d.setTranslation(-x,-y);
         camera2d.update();
-        mapRenderer.setView(camera2d.combined4,-x-width/2,-y-height/2,width,height);
+        mapRenderer.setView(camera2d.combined4,x-width/2,y-height/2,width,height);
 
-//        map.getLayers().get("back1").setOffsetX(-x/10);
-//        map.getLayers().get("back1").setOffsetY(y/10);
-//        map.getLayers().get("front1").setOffsetX(x/10);
-//        map.getLayers().get("front1").setOffsetY(-y/10);
         mapRenderer.render();
+        shapeRenderer.setProjectionMatrix(camera2d.combined4);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        player.debugDraw(shapeRenderer);
+        shapeRenderer.end();
+
+        if(Gdx.input.isKeyPressed(Input.Keys.F3)){
+            shapeRenderer.setProjectionMatrix(camera2d.combined4);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            player.debugDraw(shapeRenderer);
+            for(RectBody wall : walls){
+                wall.debugDraw(shapeRenderer);
+            }
+            shapeRenderer.end();
+        }
     }
 
     @Override
@@ -56,8 +109,6 @@ public class TestScreen extends BasicScreen {
 
     @Override
     public void resize(int width, int height) {
-        width=Gdx.graphics.getWidth();
-        height=Gdx.graphics.getHeight();
-        camera2d.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        camera2d.setSize(width,height);
     }
 }
